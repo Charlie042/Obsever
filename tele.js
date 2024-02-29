330c3b95e0db872b503493ced488332fa113a153
Charles042 <ekechukwuma20@gmail.com>
2024-02-29 21:22:55 +0100

    Telephone class created

--- tele.js
+++ tele.js
@@ -1,5 +1,19 @@
 class telephone{
     constructor(){
-        const phoneNum = new Set();
+         this.phoneNum = new Set();
+    }
+    AddPhoneNumber(num){
+        this.phoneNum.add(num);
+    }
+    RemovePhoneNumber(num){
+        this.phoneNum.delete(num);
+    }
+    DialPhoneNumber(num){
+        if(this.phoneNum.has(num)){
+            console.log(`${num} is dailing`)
+        }else{
+            console.log("Phone number not found")
+        }
     }
 }
+{{ }}
