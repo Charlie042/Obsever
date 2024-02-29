ad215a88c38a3ba0d8f0e40ed9dea6c35f0924a2
Charles042 <ekechukwuma20@gmail.com>
2024-02-29 21:29:59 +0100

    Addded a Observer class with add remove and notify

--- tele.js
+++ tele.js
@@ -1,6 +1,8 @@
+
 class telephone{
     constructor(){
          this.phoneNum = new Set();
+         this.observers = []
     }
     AddPhoneNumber(num){
         this.phoneNum.add(num);
@@ -14,6 +16,16 @@ class telephone{
         }else{
             console.log("Phone number not found")
         }
+    }
+    addObserver(num){
+        this.observers.push(num)
+    }
+    removeObserver(num){
+        this.observers = this.observers.filter((observer) => observer !== num);
+    }
+    notify(data){
+        this.observers.forEach((observer) => observer(data));
+
     }
 }
 {{ }}
